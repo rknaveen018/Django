15864576c6eb4c118cb69af1976bffed64feaff8
rknavee018 <rknaveen018@gmail.com>
2024-08-19 15:37:20 +0530

    fixed input error.

--- learning/newsite/views.py
+++ learning/newsite/views.py
@@ -3,35 +3,37 @@ from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseNotFou
 from django.urls import reverse
 
 # Create your views here.
-actions = {'1':'add', '2':'sub', '3':'div', '4':'mul'}
+actions = {'1':'Addition', '2':'Subtraction', '3':'Division', '4':'Multiple'}
 
 def home(request):
     return render(request, "home.html", {'actions': actions.values})
 
 def action(request, val):
-    return render(request, "action.html", {'action': val})
+    return render(request, "action.html", {'action': val.title()})
 
 def result(request,val):
 
     try:
-        num1 = int(request.GET.get('num1'))
-        num2 = int(request.GET.get('num2'))
+        # Get numbers from the request, with default values if not provided
+        num1 = int(request.GET.get('num1', 0))
+        num2 = int(request.GET.get('num2', 0))
     
-        if val == "add":
+        # Perform the requested operation
+        if val == "Addition":
             res = num1 + num2
-        elif val == "sub":
+        elif val == "Subtraction":
             res = num1 - num2
-        elif val == "mul":
+        elif val == "Multiple":
             res = num1 * num2
-        elif val == "div":
-            res = num1 / num2 
+        elif val == "Division":
+            if num2 == 0:
+                return HttpResponse("Error: Division by zero is not allowed.")
+            res = num1 / num2
         else:
             pass
-
+    
         return render(request, "result.html", {'result': res})
     
     except:
-        return HttpResponse("Please give your input")
-
-    
+        return HttpResponse("Invalid input/Empty input. Please provide valid numbers.")
     
